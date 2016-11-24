2c32b0a002cba2f56e5adc14abbcae3170150f4b
Dendy Gema <Zenaki Kurniawan>
2016-11-24 10:22:32 +0700

    MVVM

---
+++ ext/packages/sencha-soap/architect/SoapReader.Definition.js
@@ -0,0 +1,13 @@
+{
+    "classAlias" : "reader.soap",
+    "className"  : "Ext.data.soap.Reader",
+    "inherits"   : "Ext.data.reader.Xml",
+    "autoName"   : "SoapReader",
+    "helpText"   : "A Soap Reader",
+
+    "toolbox" : {
+        "name"     : "Soap Reader",
+        "category" : "Data Readers",
+        "groups"   : ["Data"]
+    }
+}
