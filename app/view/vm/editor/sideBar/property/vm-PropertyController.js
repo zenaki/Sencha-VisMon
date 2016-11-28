3e3ce0f59cf71208bb2851abbfb152b8eaf34609
Dendy Gema <Zenaki Kurniawan>
2016-11-28 12:08:26 +0700

    Save Visual Monita

--- app/view/vm/editor/sideBar/property/vm-PropertyController.js
+++ app/view/vm/editor/sideBar/property/vm-PropertyController.js
@@ -5,8 +5,8 @@ Ext.define('VisualMonita.view.vm.editor.sideBar.property.vm-PropertyController',
   onPropertyChange: function(source, recordId, value, oldValue, eOpts) {
       // console.log('source = '); console.log(source);
       // console.log('recordId = ' + recordId + ' value = ' + value + ' oldValue = ' + oldValue);
-      var object = Ext.getCmp(source.label_1);
-      if (recordId == 'label_1') object = Ext.getCmp(oldValue);
+      var object = Ext.ComponentQuery.query('#'+source.label_1)[0];
+      if (recordId == 'label_1') object = object = Ext.ComponentQuery.query('#'+oldValue)[0];
       object.setPosition(source.label_4, source.label_5, true);
       object.setSize(source.label_3, source.label_2);
       object.setConfig('itemId', source.label_1);
